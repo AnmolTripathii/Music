72bf50768c356581d9cc786295b98d47a6ad9a52
Anmol Tripathi <145045595+AnmolTripathii@users.noreply.github.com>
2024-08-05 17:16:08 +0530

    Update page.tsx

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Image from "next/image";
-import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import courseData from "@/data/music_courses.json"
 interface Course{
